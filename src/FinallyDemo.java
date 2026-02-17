d3380b02da26a2390e84ea3add3863319cce555e
amrita-m-pillai <amritampillai06@gmail.com>
2026-02-17 17:52:39 +0530

    first commit

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
 
 public class FinallyDemo {
-    
-         public static void main(String args[])
-        {
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter two integers");
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            try{
-                int z=a/b;
-                System.out.println("Result: "+z);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-            }
-            catch (ArithmeticException e)
-            {
-                System.out.println("Error");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-            }
-            finally
-            {
-                System.out.println("Finally block executed");
-            }
-
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error");
+        } finally {
+            System.out.println("Finally block executed");
         }
+
+        sc.close();
+    }
 }
